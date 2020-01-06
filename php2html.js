31692321b9702385efc81fb2f68b93eac79ead3b
dkz <dkz@gmail.com>
2020-01-05 17:43:52 -0800

    dkz

---
+++ php2html.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+// twillio : 1 201 367 1972
+
+
+const fs = require('fs-extra');
+const path = require('path')
+const walk = require('klaw-sync')
+const input_folder = '../224-co.th';
+const output_folder = './html'
+
+function rebuild(lang) {
+  // get code to include.
+  const head = fs.readFileSync(path.join(input_folder,lang,'head.php'),'utf8')
+  .replace(/<\?= base_url; \?>/g,'../') // must be first
+  .replace(/<\?php.*\?>/s,'')
+  console.log(head)
+
+  const header = fs.readFileSync(path.join(input_folder,lang,'header.php'),'utf8')
+  .replace(/<\?= base_url; \?>/g,'../') // must be first
+//  .replace(/<\?php.*\?>/s,'')
+  console.log(header)
+
+  const footer = fs.readFileSync(path.join(input_folder,lang,'footer.php'),'utf8')
+  .replace(/<\?= base_url; \?>/g,'../') // must be first
+//  .replace(/<\?php.*\?>/s,'')
+  console.log(footer)
+
+  const sidebar = fs.readFileSync(path.join(input_folder,lang,'sidebar.php'),'utf8')
+  .replace(/<\?= base_url; \?>/g,'../') // must be first
+//  .replace(/<\?php.*\?>/s,'')
+  console.log(sidebar)
+
+
+  // each php file from either en or th folders.
+
+  walk(path.join(input_folder,lang)).forEach(file =>{
+    if (file.path.endsWith('.php')) {
+      console.log('==================\n'+file.path+'\n=====================')
+      const html = fs.readFileSync(file.path,'utf8')
+      .replace(/<\?= base_url; \?>/g,'../') // must be first
+      .replace(/<\?php include\('head.php'\); \?>/g, head)
+      .replace(/<\?php include\('header.php'\); \?>/g, header)
+      .replace(/<\?php include\('footer.php'\); \?>/g, footer)
+      .replace(/<\?php include\('sidebar.php'\); \?>/g, sidebar)
+      //console.log(html)
+
+      fs.outputFileSync(file.path.replace(/\.php$/,'.html'),html);
+    }
+  })
+
+
+
+} // rebuild.
+
+rebuild('en');
+rebuild('th');
